a76e65ac1bdaff358f1acaec8b214a1d3949237d
zzj <215179993@qq.com>
2020-12-16 22:19:41 +0800

    game

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,69 @@
-import React,{Component} from 'react'
-import {connect} from 'dva'
+import React, { Component } from 'react'
+import { connect } from 'dva'
+type TImgName = 'backgroundImg' | 'monsterImg' | 'humterImg'
+type TImgReadyName = 'backgroundReady' | 'monsterReady' | 'humterReady'
+interface Role {
+  x: number,
+  y: number,
+}
 
- class Index extends Component {
+class Humter implements Role {
+  x: number;
+  y: number;
+  speed: number;
+  constructor(x: number, y: number, speed: number) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed
+  }
+  move() {
+
+  }
+}
+
+class Monster implements Role {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+}
+
+class Game {
+  backgroundReady: boolean = false
+  monsterReady: boolean = false
+  humterReady: boolean = false
+  backgroundImg: HTMLImageElement | null = null
+  monsterImg: HTMLImageElement | null = null
+  humterImg: HTMLImageElement | null = null
+  context: CanvasRenderingContext2D | null = null
+  // 创建画布
+  createCanvas() {
+    let canvas = document.getElementById('canvas')
+    if (canvas instanceof HTMLCanvasElement) {
+      this.context = canvas.getContext('2d')
+      canvas.width = 512;
+      canvas.height = 480;
+    }
+  }
+  // 初始化图片
+  initImg(imgName: TImgName, isImgReadyName: TImgReadyName) {
+    this[imgName] = new Image()
+    if (this[imgName] instanceof HTMLImageElement) {
+      this[imgName].onload = () => {
+        this[isImgReadyName] = true
+      }
+      this[imgName].src = require('../asserts/images/background.png')
+    }
+  }
+}
+
+class Index extends Component {
   render() {
     return (
       <div>
-        zzj
+        <canvas id='canvas' ></canvas>
       </div>
     )
   }
