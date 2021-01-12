9ef4512f73258923c7e9aea4c2cdd0adf370767e
zzj <215179993@qq.com>
2021-01-12 21:18:10 +0800

    one-handle

---
+++ src/utils/oneHandle.js
@@ -0,0 +1,53 @@
+'use strict'
+
+/**
+ * @description: 多次调用，响应一次，可开缓存
+ * @param {Function} fn 一个返回promise的函数
+ * @param {Boolean} cache 是否开启缓存
+ * @param {*} context 上下文
+ * @returns {Function} return Promise的闭包
+ */
+
+function oneHandle(fn, cache, context) {
+  const queueThen = []
+  const queueCatch = []
+  let cacheData
+  return function () {
+    return new Promise((resolve, reject) => {
+      if (cacheData !== undefined) {
+        resolve(cacheData)
+      } else if (queueThen.length) {
+        queueThen.push(resolve)
+        queueCatch.push(reject)
+      } else {
+        queueThen.push(resolve)
+        queueCatch.push(reject)
+        fn.apply(context, arguments).then(data => {
+          if (cache) {
+            cacheData = data
+          }
+          queueThen.forEach(then => then(data))
+          queueThen.length = 0
+          queueCatch.length = 0
+        }).catch(err => {
+          queueCatch.forEach(then => then(err))
+          queueCatch.length = 0
+          queueThen.length = 0
+        })
+      }
+    })
+  }
+}
+
+function wait(time,data=0){
+    return new Promise(resolve=>{
+        setTimeout(()=>resolve(data),time)
+    })
+}
+
+const $wait1=oneHandle(wait)
+console.log($wait1.toString())
+
+if ('undefined' !== typeof module) {
+  module.exports = oneHandle
+}
