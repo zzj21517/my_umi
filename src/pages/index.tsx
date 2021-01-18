9d92a0f51db535e6b158798df1ff48d5df2d7e95
zhangzj <zhangzj@51youse.com>
2021-01-18 17:17:46 +0800

    学习debounce

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'umi';
 import { connect } from 'dva';
-
+import { debounce } from '@/utils/debounce'
+import styles from './index.less'
+let count = 0
 class Index extends Component {
+    constructor(props: {} | Readonly<{}>) {
+        super(props)
+        this.move = debounce(this.move,1000, true)
+    }
+    move = (dom: Element) => {
+        console.log('move')
+        count++
+        dom.innerHTML = String(count)
+    }
+    componentDidMount() {
+        let container = document.querySelector('#container')
+        container?.addEventListener('mousemove', () => this.move(container!))
+        console.log(container, 'ccc')
+    }
     render() {
         return (
             <div>
                 <Link to="/game">游戏</Link>
                 <br />
                 <Link to="/process">进度条</Link>
-                <br/>
+                <br />
                 <Link to="/componentLearn">组件学习</Link>
-                <br/>
+                <br />
                 <Link to="/myTable">table学习</Link>
+
+                <div id='container' className={styles.container}></div>
             </div>
         );
     }
