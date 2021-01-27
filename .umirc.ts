f593f1a20d72fa4d1f95d56912e29486317aca58
zzj <215179993@qq.com>
2021-01-27 22:49:20 +0800

    plugin

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-06 20:14:15
- * @LastEditTime: 2021-01-27 22:19:15
+ * @LastEditTime: 2021-01-27 22:41:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \umiapp\.umirc.ts
@@ -9,7 +9,9 @@
 import { defineConfig } from 'umi';
 const CheckCompilerHooksPlugin = require('./plugins/checkCompilerHooksPlugin')
 export default defineConfig({
-  plugins: [new CheckCompilerHooksPlugin()],
+  chainWebpack(config, { env, webpack, createCSSRule }) {
+    config.plugin('CheckCompilerHooksPlugin').use(CheckCompilerHooksPlugin)
+  },
   hash: true,
   history: {
     type: 'hash',
