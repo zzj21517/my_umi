f567fb2e2d60a37379df33ba98017a03a7942a3d
zhangzj <zhangzj@51youse.com>
2021-02-05 17:12:15 +0800

    learnhook

---
+++ src/pages/learnHook/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-05 14:25:09
+ * @LastEditTime: 2021-02-05 17:11:55
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /my_umi/src/pages/learnHook/index.tsx
+ */
+
+
+import React, { useState, useEffect } from 'react'
+
+export default function UserFriendStatus() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
