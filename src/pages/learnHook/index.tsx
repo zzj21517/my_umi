a9fa820d8d46f68d576d6322530c6bac97bacded
zzj <215179993@qq.com>
2021-02-13 17:49:35 +0800

    hook

--- src/pages/learnHook/index.tsx
+++ src/pages/learnHook/index.tsx
@@ -1,31 +1,51 @@
 /*
  * @Author: your name
  * @Date: 2021-02-05 14:25:09
- * @LastEditTime: 2021-02-13 11:44:00
+ * @LastEditTime: 2021-02-13 12:49:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /my_umi/src/pages/learnHook/index.tsx
  */
 
 
-import React, { useState, useEffect, Component } from 'react'
+import React, { useState, useRef, useEffect, Component } from 'react'
 import ComWithLoading from '@/components/hocCom'
 import RenderPropsCom from '@/components/renderPropsCom'
 import HookCom from '@/components/hookCom'
 function UserFriendStatus(props: { data: Array<string> }) {
+    const [count, setCount] = useState(0)
+    const likeRef = useRef(0)
     const { data } = props
+    const handleAlert = () => {
+        setTimeout(() => {
+            alert(count);
+            alert(likeRef.current);
+        }, 2000);
+    }
     return (
         <div>
-            <div style={{color:'red'}} >hoc</div>
+            <div style={{ color: 'red' }} >hoc</div>
             {
                 data.map(item => <div key={item} >{item}</div>)
             }
-            <div style={{color:'red'}} >render props</div>
+            <div style={{ color: 'red' }} >render props</div>
             <RenderPropsCom render={
                 (item: string) => <div>{item}</div>
             } />
-            <div style={{color:'red'}} >hook</div>
+            <div style={{ color: 'red' }} >hook</div>
             <HookCom />
+
+
+
+            <div>button</div>
+            <button onClick={() => {
+                setCount(count + 1)
+                likeRef.current++
+            }} >+1</button>
+            <br />
+            <button onClick={
+                handleAlert
+            } >alert</button>
         </div>
     )
 }
