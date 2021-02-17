19c67697c80004b7966f9e94c56cfd69aa84bbe3
zzj <215179993@qq.com>
2021-02-17 22:04:15 +0800

    learnpromise

--- src/pages/learnPromise/index.tsx
+++ src/pages/learnPromise/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-17 20:29:14
- * @LastEditTime: 2021-02-17 20:34:52
+ * @LastEditTime: 2021-02-17 21:36:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \umiapp\src\pages\learnPromise\index.tsx
@@ -11,7 +11,16 @@ import React, { Component } from 'react'
 import { CustomizePromise } from '@/utils/customizePromise'
 export default class LearnPromise extends Component {
     componentDidMount() {
-        new CustomizePromise().then()
+        new CustomizePromise((resolve: Function, reject: Function) => {
+            setTimeout(() => {
+                console.log('a')
+                resolve('b')
+            })
+        }).then(() => {
+            console.log('success')
+        }, () => {
+            console.log('error')
+        })
     }
     render() {
         return (
--- src/utils/customizePromise.ts
+++ src/utils/customizePromise.ts
@@ -1,19 +1,60 @@
 /*
  * @Author: your name
  * @Date: 2021-02-17 20:29:43
- * @LastEditTime: 2021-02-17 20:34:29
+ * @LastEditTime: 2021-02-17 21:50:34
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \umiapp\src\utils\customizePromise.ts
  */
-
+let allStatus = {
+    PENDING: Symbol.for('PENGDING'),
+    RESOLVE: Symbol.for('RESOLVE'),
+    REJECT: Symbol.for('REJECT')
+}
 export class CustomizePromise {
-    constructor() {
-        setTimeout(() => {
-            console.log('a')
-        })
+    status: symbol
+    resolveCallbackList: Array<Function> = []
+    rejectCallbackList: Array<Function> = []
+    value: any = undefined
+    err: any = undefined
+    constructor(executor: Function) {
+        this.status = allStatus.PENDING
+        this.resolve=this.resolve.bind(this)
+        this.reject=this.reject.bind(this)
+        try {
+            executor(this.resolve, this.reject)
+        } catch (err) {
+            this.reject(err)
+        }
     }
-    then() {
-        console.log('then')
+    resolve(res: any) {
+        if (this.status === allStatus.PENDING) {
+            this.value = res
+            this.status = allStatus.RESOLVE
+            this.resolveCallbackList.forEach(fn => fn())
+        }
+    }
+    reject(err: any) {
+        if (this.status === allStatus.PENDING) {
+            this.err = err
+            this.status = allStatus.REJECT
+            this.rejectCallbackList.forEach(fn => fn())
+        }
+    }
+    then(resolved: Function, rejected: Function) {
+        if (this.status === allStatus.RESOLVE) {
+            resolved(this.value)
+        }
+        if (this.status === allStatus.REJECT) {
+            rejected(this.err)
+        }
+        if (this.status === allStatus.PENDING) {
+            this.resolveCallbackList.push(() => {
+                resolved(this.value)
+            })
+            this.rejectCallbackList.push(() => {
+                rejected(this.err)
+            })
+        }
     }
 }
