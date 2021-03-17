d7a823a9bcc2577a99aa5bafb05a423c0d135d46
zhangzj <zhangzj@51youse.com>
2021-03-17 17:36:31 +0800

    rxjs

--- src/pages/learnRxjs/index.tsx
+++ src/pages/learnRxjs/index.tsx
@@ -1,77 +1,38 @@
 /*
  * @Author: your name
- * @Date: 2021-01-13 21:20:10
- * @LastEditTime: 2021-03-11 10:08:20
+ * @Date: 2021-03-17 16:08:27
+ * @LastEditTime: 2021-03-17 17:31:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \umiapp\src\pages\learnRxjs\index.tsx
+ * @FilePath: /my_umi/src/pages/learnRxjs/index.tsx
  */
-import React, { Component } from 'react';
-import { Tabs } from 'antd';
-import { fromEvent, interval, timer, of, from, Subject } from 'rxjs';
-import {
-    switchMap,
-    take,
-    map,
-    combineAll,
-    zip,
-    scan,
-    mapTo,
-    throttleTime,
-    delay,
-    mergeAll,
-} from 'rxjs/operators';
-import moment from 'moment';
-import shortid from 'shortid'
-export default class LearnRxJs extends Component {
-    constructor(props: any) {
-        super(props);
-    }
-    state = {};
-    componentDidMount() {
-        let click = fromEvent(document.body, 'click')
-        let source = click.pipe(map(e => interval(1000)))
-        let example = source.pipe(mergeAll())
-        example.subscribe(res => console.log(res))
-    }
-
-    rxjs1() {
-        console.log('rxjs1');
-        var button = document.querySelector('.btnzzj');
-        console.log(button, 'bbb');
-        // button?.addEventListener('click', () => {
-        //     console.log('clicked!')
-        // })
-        fromEvent(button!, 'click')
-            .pipe(
-                throttleTime(1000),
-                map(event => event.clientX),
-                scan((count, value, index) => {
-                    console.log(count, value, index);
-                    return count + 1;
-                }, 0),
-            )
+import React, { useState, useEffect } from 'react'
+import { fromEvent, interval, combineLatest } from 'rxjs'
+import { mapTo, scan, switchMapTo, takeUntil, tap, startWith, map } from 'rxjs/operators'
+export default function index() {
+    useEffect(() => {
+        let btnStop = document.getElementById('btnStop')
+        let btnEnlarge = document.getElementById('btnEnlarge')
+        let btnAdd = document.getElementById('btnAdd')
+        let btnReduce = document.getElementById('btnReduce')
+        let stop$ = interval(2000).pipe(takeUntil(fromEvent(btnStop!, 'click')))
+        let start = fromEvent(btnEnlarge!, 'click').pipe(
+            switchMapTo(stop$), scan((prev, cur) => prev + 1, 0)
+        )
+        let add = fromEvent(btnAdd!, 'click').pipe(mapTo(1), startWith(0), scan((prev, cur) => prev + cur, 0))
+        let reduce = fromEvent(btnReduce!, 'click').pipe(mapTo(-1), startWith(0), scan((prev, cur) => prev + cur, 0))
+        combineLatest([start, add, reduce])
+            // .pipe(map(([a, b, c]: Array<number>) => a + b + c))
             .subscribe(res => {
-                console.log('clicked', res);
-            });
-    }
-    render() {
-        return (
-            <div>
-                <Tabs defaultActiveKey="1">
-                    <Tabs.TabPane tab="Tab 1" key="1">
-                        1
-          </Tabs.TabPane>
-                    <Tabs.TabPane tab="Tab 2" key="2">
-                        2
-          </Tabs.TabPane>
-                    <Tabs.TabPane tab="Tab 3" key="3">
-                        3
-          </Tabs.TabPane>
-                </Tabs>
-
-                <button className="btnzzj">zzzz</button>
-            </div>
-        );
-    }
+                console.log(res)
+            })
+    }, [])
+    return (
+        <div>
+            <button id='btnStop' >停止</button>
+            <button id='btnEnlarge'>放大</button>
+            <button id='btnAdd'>++</button>
+            <button id='btnReduce'>--</button>
+        </div>
+    )
 }
