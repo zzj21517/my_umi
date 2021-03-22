9198b4231d7a4b624734e25e814ebfbfdcb35dec
zzj <215179993@qq.com>
2021-03-22 22:23:44 +0800

    rxjs

--- src/pages/learnRxjs/index.tsx
+++ src/pages/learnRxjs/index.tsx
@@ -1,38 +1,46 @@
 /*
  * @Author: your name
  * @Date: 2021-03-17 16:08:27
- * @LastEditTime: 2021-03-17 17:31:56
+ * @LastEditTime: 2021-03-22 22:22:23
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /my_umi/src/pages/learnRxjs/index.tsx
  */
 import React, { useState, useEffect } from 'react'
-import { fromEvent, interval, combineLatest } from 'rxjs'
-import { mapTo, scan, switchMapTo, takeUntil, tap, startWith, map } from 'rxjs/operators'
+import { fromEvent, interval, combineLatest, timer } from 'rxjs'
+import { mapTo, scan, switchMapTo, takeUntil, tap, startWith, map, switchMap, takeWhile } from 'rxjs/operators'
 export default function index() {
-    useEffect(() => {
-        let btnStop = document.getElementById('btnStop')
-        let btnEnlarge = document.getElementById('btnEnlarge')
-        let btnAdd = document.getElementById('btnAdd')
-        let btnReduce = document.getElementById('btnReduce')
-        let stop$ = interval(2000).pipe(takeUntil(fromEvent(btnStop!, 'click')))
-        let start = fromEvent(btnEnlarge!, 'click').pipe(
-            switchMapTo(stop$), scan((prev, cur) => prev + 1, 0)
-        )
-        let add = fromEvent(btnAdd!, 'click').pipe(mapTo(1), startWith(0), scan((prev, cur) => prev + cur, 0))
-        let reduce = fromEvent(btnReduce!, 'click').pipe(mapTo(-1), startWith(0), scan((prev, cur) => prev + cur, 0))
-        combineLatest([start, add, reduce])
-            // .pipe(map(([a, b, c]: Array<number>) => a + b + c))
-            .subscribe(res => {
-                console.log(res)
-            })
-    }, [])
-    return (
-        <div>
-            <button id='btnStop' >停止</button>
-            <button id='btnEnlarge'>放大</button>
-            <button id='btnAdd'>++</button>
-            <button id='btnReduce'>--</button>
-        </div>
-    )
+  useEffect(() => {
+    let inputDom = document.getElementById('input')
+    let updateDom = document.getElementById('update')
+    function compareNum(cur: number, old: number) {
+      return cur > old ? 1 : -1
+    }
+    function takeUntilFunc(cur: number, old: number) {
+      return cur > old ? (val: number) => val >= old : (val: number) => val <= old
+    }
+    let subscription = (function (old) {
+      fromEvent(updateDom!, 'click').pipe(
+        map(_ => (inputDom as any).value),
+        switchMap(cur => {
+          return timer(0, 20).pipe(
+            mapTo(compareNum(cur, old)),
+            startWith(old),
+            scan((prev, curv) => prev + curv),
+            takeWhile(takeUntilFunc(cur, old))
+          )
+        }),
+        tap(v => (old = v)),
+        startWith(old)
+      ).subscribe(res => {
+        console.log(res)
+      })
+    })(0)
+  }, [])
+  return (
+    <div>
+      <input id='input' type="text" />
+      <button id='update' >更新</button>
+    </div>
+  )
 }
