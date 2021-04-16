c7e206e8c998cfa19d5163187d12e57589e70948
zhangzj <zhangzj@51youse.com>
2021-04-16 09:50:23 +0800

    学习canvas

--- src/pages/learnCanvas/index.tsx
+++ src/pages/learnCanvas/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-15 10:32:26
- * @LastEditTime: 2021-04-15 18:33:06
+ * @LastEditTime: 2021-04-16 09:50:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /my_umi/src/pages/learnCanvas/index.tsx
@@ -35,26 +35,32 @@ export default function LearnCanvas() {
 
     function drawCanvas(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
         context.save()
+        let count = 0
         context.font = '20px SourceHanSerifCN-Medium'
         context.fillStyle = '#323233'
         context.textAlign = 'center'
-        context.fillText('这里套餐名称', canvasWidth / 2, 48)
+        context.fillText('这里套餐名称', canvasWidth / 2, count = count + 48)
         context.font = '16px SourceHanSerifCN-Medium'
-        context.fillText('有效期：开通后3日内有效', canvasWidth / 2, 75)
+        context.fillText('有效期：开通后3日内有效', canvasWidth / 2, count = count + 27)
         let img = new Image()
         img.onload = (e) => {
-            context.drawImage(img, (canvasWidth - 200) / 2, 88, 200, 200)
-
+            context.drawImage(img, (canvasWidth - 180) / 2, count = count + 13, 180, 180)
+            count = count + 180 + 15
             context.font = '14px PingFangSC-Regular'
             context.textAlign = 'left'
-            context.fillText('使用方法：', 40, 330)
-            context.fillText('1、微信关注公众号【中国矿业报】', 40, 354)
-            context.fillText('2、点击公众号底部【数字报刊】，进入矿业报；', 40, 378)
-            context.fillText('3、点击底部【我的】，再点右上角【扫一扫】', 40, 402)
-            context.fillText('4、扫描该二维码，即可领取', 40, 426)
-
+            context.fillText('使用方法：', 40, count = count + 24)
+            context.fillText('1、微信关注公众号【中国矿业报】', 40, count = count + 24)
+            context.fillText('2、点击公众号底部【数字报刊】，进入矿业报；', 40, count = count + 24)
+            context.fillText('3、点击底部【我的】，再点右上角【扫一扫】', 40, count = count + 24)
+            context.fillText('4、扫描该二维码，即可领取', 40, count = count + 24)
+            console.log(context.measureText('2、点击公众号底部【数字报刊】，进入矿业报；'))
             context.fillStyle = 'rgba(255,244,244,1)'
-            context.fillRect(0, 450, 375, 530)
+            context.fillRect(0, count = count + 17, canvasWidth, canvasHeight)
+            context.fillStyle = '#323233'
+            context.font = '20px SourceHanSerifCN-Medium'
+            context.fillText('筑梦2020', 40, count = count + 53)
+            context.font = '14px PingFangSC-Regular'
+            context.fillText('邀您一起看矿业报，最新报刊数据随时看', 40, count = count + 29)
             setImgSrc(canvas.toDataURL())
         }
         img.src = require('@/asserts/images/cat.jpg')
@@ -67,7 +73,6 @@ export default function LearnCanvas() {
             <div className='content' >
                 <div className="content_header">这里套餐名称</div>
                 <div className="content_expire">有效期：开通后3日内有效</div>
-                <div>{window.devicePixelRatio}</div>
             </div>
         </div>
     )
